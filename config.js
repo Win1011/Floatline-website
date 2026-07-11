373bf4f4e40b3f7de1e7f4a9ba18bc921967844a
Win1011 <1354312495@qq.com>
2026-07-11 09:18:54 +0800

    feat: add Floatline lifetime purchase website flow

---
+++ config.js
@@ -0,0 +1,3 @@
+// Set this to the live Dodo Payments checkout URL when the product is approved.
+// Keeping it empty makes the website show an honest "checkout is being activated" state.
+window.FLOATLINE_CHECKOUT_URL = window.FLOATLINE_CHECKOUT_URL || '';
